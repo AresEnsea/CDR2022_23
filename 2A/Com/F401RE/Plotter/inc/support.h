ee3d766d31c52c5f8f53bed8ec61f97443f832b4
HOSTEER <louis.lienart@ensea.fr>
2022-12-21 20:09:48 +0100

    ok

--- 2A/Com/F401RE/Plotter/inc/support.h
+++
@@ -1,8 +0,0 @@
-#ifndef __support
-#define __support
-
-#include <math.h>
-void ftoa(float n, char* res, int afterpoint) ;  // floating/double to string (display on LCD usage)
-int intToStr(int x, char str[], int d) ;
-
-#endif
