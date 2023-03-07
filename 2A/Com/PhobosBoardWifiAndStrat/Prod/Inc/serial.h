6fec60d587d2af9ef32141fbc1f640a919b18669
DamsoNero <damien.lefuel@ensea.fr>
2023-03-07 17:09:05 +0100

    majwifistrat

---
+++ 2A/Com/PhobosBoardWifiAndStrat/Prod/Inc/serial.h
@@ -0,0 +1,19 @@
+#ifndef _SERIAL_
+#define _SERIAL_
+
+#include "usart.h"
+#include "gpio.h"
+#include "robot.h"
+#include "lidar.h"
+#include <stdio.h>
+
+extern uint8_t lidarData;
+extern uint8_t wifiDataRX;
+extern uint8_t wifiDataTX;
+extern uint8_t armData;
+extern uint8_t pData[1];
+
+void parser32(uint32_t *ptr);
+int serial_send(uint8_t *ptr, int len, int uartPort);
+
+#endif
